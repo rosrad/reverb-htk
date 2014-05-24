e847a45c5d125b3b626c6e462ba3cc6dfffba99f
renbo <justnow.ren@gmail>
2014-05-24 21:46:09 +0900

    able to run the task list

---
+++ task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+task_list=${1:-"task.list"}
+#set -x
+while IFS= read -r line
+do
+    cat LOCAL_CONFIG.base | sed 's#\(export DT_TASK=\).*#\1'"${line}"'#g' > LOCAL_CONFIG
+    bash ./WSJBuildAndTest
+done < "$task_list"
+#set +x
