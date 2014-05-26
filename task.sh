798e44f8b7077272d8e12eaadf3b2e93c46a0dd7
renbo <justnow.ren@gmail>
2014-05-26 18:31:28 +0900

    use new tasklist

--- task.sh
+++ task.sh
@@ -3,6 +3,7 @@ task_list=${1:-"task.list"}
 #set -x
 while IFS= read -r line
 do
+    echo "============  run task ${line}  ============"
     cat LOCAL_CONFIG.base | sed 's#\(export DT_TASK=\).*#\1'"${line}"'#g' > LOCAL_CONFIG
     bash ./WSJBuildAndTest
 done < "$task_list"
